784b5847d2a11da7d842ebf6b93320ab880e9273
Paul Querna <pquerna@apache.org>
2014-11-24 11:44:26 -0800

    fast path for small numbers on Parse

--- fflib/v1/bytenum.go
+++ fflib/v1/bytenum.go
@@ -32,9 +32,57 @@ func ParseFloat(s []byte, bitSize int) (f float64, err error) {
 
 // ParseUint is like ParseInt but for unsigned numbers, and oeprating on []byte
 func ParseUint(s []byte, base int, bitSize int) (n uint64, err error) {
+	if len(s) == 1 {
+		switch s[0] {
+		case '0':
+			return 0, nil
+		case '1':
+			return 1, nil
+		case '2':
+			return 2, nil
+		case '3':
+			return 3, nil
+		case '4':
+			return 4, nil
+		case '5':
+			return 5, nil
+		case '6':
+			return 6, nil
+		case '7':
+			return 7, nil
+		case '8':
+			return 8, nil
+		case '9':
+			return 9, nil
+		}
+	}
 	return internal.ParseUint(s, base, bitSize)
 }
 
 func ParseInt(s []byte, base int, bitSize int) (i int64, err error) {
+	if len(s) == 1 {
+		switch s[0] {
+		case '0':
+			return 0, nil
+		case '1':
+			return 1, nil
+		case '2':
+			return 2, nil
+		case '3':
+			return 3, nil
+		case '4':
+			return 4, nil
+		case '5':
+			return 5, nil
+		case '6':
+			return 6, nil
+		case '7':
+			return 7, nil
+		case '8':
+			return 8, nil
+		case '9':
+			return 9, nil
+		}
+	}
 	return internal.ParseInt(s, base, bitSize)
 }
