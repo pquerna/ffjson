b13124f3370658fe9440ac95fba83b9d6adc5e70
Klaus Post <klauspost@gmail.com>
2015-02-12 21:24:26 +0100

    Convert strconv.AppendFloat to use the internal buffer for writing.

--- inception/encoder.go
+++ inception/encoder.go
@@ -177,11 +177,9 @@ func getGetInnerValue(ic *Inception, name string, typ reflect.Type, ptr bool, fo
 		ic.OutputImports[`fflib "github.com/pquerna/ffjson/fflib/v1"`] = true
 		out += "fflib.FormatBits(&scratch, buf, uint64(" + ptname + "), 10, false)" + "\n"
 	case reflect.Float32:
-		ic.OutputImports[`"strconv"`] = true
-		out += "buf.Write(strconv.AppendFloat([]byte{}, float64(" + ptname + "), 'g', -1, 32))" + "\n"
+		out += "fflib.AppendFloat(buf, float64(" + ptname + "), 'g', -1, 32)" + "\n"
 	case reflect.Float64:
-		ic.OutputImports[`"strconv"`] = true
-		out += "buf.Write(strconv.AppendFloat([]byte{}, " + ptname + ", 'g', -1, 64))" + "\n"
+		out += "fflib.AppendFloat(buf, " + ptname + ", 'g', -1, 64)" + "\n"
 	case reflect.Array,
 		reflect.Slice:
 
