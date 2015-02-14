b4cd68f4e10c043895931bc5b26b8e8c6c00772c
Klaus Post <klauspost@gmail.com>
2015-02-14 16:04:58 +0100

    Add tests.
    Conflicts:
    	tests/ff.go
    	tests/ff_test.go
    	tests/fuzz_test.go

--- tests/ff_test.go
+++ tests/ff_test.go
@@ -698,3 +698,10 @@ func TestInlineStructs(t *testing.T) {
 	testSameMarshal(t, &a, &b)
 	testCycle(t, &a, &b)
 }
+
+func TestInlineStructs(t *testing.T) {
+	a := TInlineStructs{}
+	b := XInlineStructs{}
+	testSameMarshal(t, &a, &b)
+	testCycle(t, &a, &b)
+}
