b4ea3dda1d136f979f1e2abb8429618cbfe11f1f
Klaus Post <klauspost@gmail.com>
2015-02-15 18:03:12 +0100

    Update test scripts (windows).

--- tests/bench.cmd
+++ tests/bench.cmd
@@ -1,5 +1,8 @@
 del ff_ffjson.go
 ffjson ff.go
 
-go test -benchmem -bench MarshalJSON
-go test -benchmem -bench MarshalJSONNative -cpuprofile="prof.dat" -benchtime 10s &&go tool pprof -gif tests.test.exe prof.dat >out.gif
+go test -benchmem -test.run="Nothing" -bench MarshalJSON
+go test -benchmem -test.run="Nothing" -test.bench="MarshalJSONNative" -cpuprofile="prof.dat" -benchtime 10s &&go tool pprof -gif tests.test.exe prof.dat >cpu.gif
+go test -benchmem -test.run="Nothing" -test.bench="MarshalJSONNativeReuse" -memprofile="prof.dat" -test.memprofilerate=1 -benchtime 10s &&go tool pprof -lines -alloc_objects -gif tests.test.exe prof.dat  >mem-reuse.gif
+
+go test -benchmem -test.run="Nothing" -test.bench="MarshalJSONNativePool" -memprofile="prof.dat" -test.memprofilerate=1 -benchtime 10s &&go tool pprof -lines -alloc_objects -gif tests.test.exe prof.dat  >mem-pool.gif
--- tests/t.cmd
+++ tests/t.cmd
@@ -1,2 +1,3 @@
+go install github.com/pquerna/ffjson
 del ff_ffjson.go
 go test -v github.com/pquerna/ffjson/fflib/v1 github.com/pquerna/ffjson/generator github.com/pquerna/ffjson/inception && ffjson ff.go && go test -v
