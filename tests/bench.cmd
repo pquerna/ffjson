4c10e6c3ddb41e54a9e01647060138413865a0b8
klauspost <klauspost@gmail.com>
2015-03-04 15:23:50 +0100

    Update Windows benchmark/tests.

--- tests/bench.cmd
+++ tests/bench.cmd
@@ -2,4 +2,8 @@ del ff_ffjson.go
 ffjson ff.go
 
 go test -benchmem -bench MarshalJSON
-go test -benchmem -bench MarshalJSONNative -cpuprofile="prof.dat" -benchtime 10s &&go tool pprof -gif tests.test.exe prof.dat >out.gif
+
+REM ### Bench CPU ###
+rem go test -benchmem -test.run=none -bench MarshalJSONNative -cpuprofile="cpu.dat" -benchtime 10s &&go tool pprof -gif tests.test.exe cpu.dat >out.gif
+
+
